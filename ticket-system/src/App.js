79fcbf0852abd0cdd14884e7b6940e81434cbf10
HC12 <80067515+hc12-pl@users.noreply.github.com>
2024-06-16 15:24:05 +0200

    Finished navbar

---
+++ ticket-system/src/App.js
@@ -0,0 +1,7 @@
+import Navbar from "./components/Navbar.js"
+
+export default function App() {
+  return (
+    <Navbar></Navbar>
+  )
+}
---
+++ ticket-system/src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <h1 id="name">Ticket System</h1>
+            <p id="faq" className="button">FAQ</p>
+            <p id="contact" className="button">Contact</p>
+            <p id="create-new-ticket" className="button">Create new Ticket</p>
+            <p id="login" className="button">Login</p>
+        </nav>
+    )
+}
