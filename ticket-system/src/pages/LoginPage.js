f02994d7b3473d231f474ae288f42a4a337a1b86
HC12 <kasiamariuszszymon0@gmail.com>
2024-06-20 00:07:18 +0200

    Finished login page (frontend)

--- ticket-system/src/pages/LoginPage.js
+++ ticket-system/src/pages/LoginPage.js
@@ -1,23 +1,30 @@
 import React from "react";
 import '../styles/loginPage.css'
 
-
 function LoginPage() {
   return (
+    <>
     <main>
-      <h1>Login Page</h1>
+      <h1>Welcome back!</h1>
       <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <section id="username">
+          <span class="material-symbols-outlined">person</span>
+          <input id="usernameInput" type="text" placeholder="Username" />
+        </section> 
+        <section id="password">
+          <span class="material-symbols-outlined">key</span>
+          <input id="passwordInput" type="password" placeholder="Password" />
+        </section>
+        <button id="submitBtn">Login</button>
       </form>
       <p>
         Don't have an account? Contact Us!
       </p>
       <p>
-        <a href="/forgot-password">Forgot Password?</a>
+        <a id="forgotPswd" href="/forgot-password">Forgot Password?</a>
       </p>
     </main>
+    </>
   );
 }
 
