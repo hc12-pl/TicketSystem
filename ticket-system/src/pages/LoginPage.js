9b4557c1d4e6d919ea34f2c999c77f2433f48516
HC12 <80067515+hc12-pl@users.noreply.github.com>
2024-07-07 13:24:28 +0200

    Added functionality to show and hide password

--- ticket-system/src/pages/LoginPage.js
+++ ticket-system/src/pages/LoginPage.js
@@ -1,29 +1,45 @@
 import React from "react";
-import '../styles/loginPage.css'
+import '../styles/loginPage.css';
 
 function LoginPage() {
+  let showPassword = false;
+  const handleShowPasswordClick = () => {
+    const passwordInput = document.getElementById("passwordInput");
+    const passwordShowIcon = document.getElementById("togglePassword")
+    showPassword = !showPassword
+    if (showPassword) {
+      passwordInput.setAttribute('type', 'text')
+      passwordShowIcon.innerHTML = "visibility_off"
+    } else {
+      passwordInput.setAttribute('type', 'password')
+      passwordShowIcon.innerHTML = "visibility"
+    }
+    
+  }
+
   return (
     <>
-    <main>
-      <h1>Welcome back!</h1>
-      <form>
-        <section id="username">
-          <span class="material-symbols-outlined">person</span>
-          <input id="usernameInput" type="text" placeholder="Username" />
-        </section> 
-        <section id="password">
-          <span class="material-symbols-outlined">key</span>
-          <input id="passwordInput" type="password" placeholder="Password" />
-        </section>
-        <button id="submitBtn">Login</button>
-      </form>
-      <p>
-        Don't have an account? Contact Us!
-      </p>
-      <p>
-        <a id="forgotPswd" href="/forgot-password">Forgot Password?</a>
-      </p>
-    </main>
+      <main>
+        <h1>Welcome back!</h1>
+        <form>
+          <section id="username">
+            <span className="material-symbols-outlined">person</span>
+            <input id="usernameInput" type="text" placeholder="Username" />
+          </section>
+          <section id="password">
+            <span className="material-symbols-outlined">key</span>
+            <input id="passwordInput" type="password" placeholder="Password" />
+            <span id="togglePassword" className="material-symbols-outlined showPassword" onClick={handleShowPasswordClick}>visibility</span>
+          </section>
+          <button type="submit" id="submitBtn">Login</button>
+        </form>
+        <p>
+          Don't have an account? Contact Us!
+        </p>
+        <p>
+          <a id="forgotPswd" href="/forgot-password">Forgot Password?</a>
+        </p>
+      </main>
     </>
   );
 }
